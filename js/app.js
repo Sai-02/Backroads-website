36883479801e49215417ee346ef7ada5dc4b0023
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2020-10-02 14:49:00 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,29 +9,29 @@ const date = (document.getElementById(
 const navBtn = document.getElementById("nav-toggle");
 const links = document.getElementById("nav-links");
 // add event listener
-navBtn.addEventListener("click",() => {
+navBtn.addEventListener("click", () => {
   links.classList.toggle("show-links");
 });
 
 // ********** smooth scroll ************
 // select links
 const scrollLinks = document.querySelectorAll(".scroll-link");
-scrollLinks.forEach(link => {
-  link.addEventListener("click", e => {
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
     // prevent default
     e.preventDefault();
     links.classList.remove("show-links");
 
-    const id = e.target.getAttribute("href").slice(2);
-    const element=document.getElementById(id);
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
     //
-    let position=element.offsetTop - 62;
+    let position = element.offsetTop - 62;
 
     window.scrollTo({
-      left:0,
+      left: 0,
       //top: element.offsetTop,
       top: position,
-      behavior: "smooth"
+      behavior: "smooth",
     });
   });
 });
